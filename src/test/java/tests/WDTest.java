baf032237b2107dd09ffb00d42ea2d6d5a5c0456
unknown <sahur@IN1W7L-522261.ttg.global>
2014-09-28 13:04:44 +0530

    committing

--- src/test/java/tests/WDTest.java
+++ src/test/java/tests/WDTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
@@ -12,54 +11,44 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import Wrapper.Wrapper;
+
 public class WDTest {
 
-	WebDriver driver=null;
-	
+	WebDriver driver = null;
+
 	@BeforeTest
-	public void beforeTestSetup(){
+	public void beforeTestSetup() {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setBrowserName("firefox");
 		capabilities.setPlatform(Platform.WINDOWS);
-		 driver = new FirefoxDriver(capabilities);
-		
+		driver = new FirefoxDriver(capabilities);
+
 	}
-	
-	
-	@Test(dataProvider="DP1")
-	public void startTest(String url) 
-	{
-	
+
+	@Test(dataProvider = "DP1")
+	public void startTest(String url) {
+
 		driver.get(url);
 		driver.manage().window().maximize();
 		System.out.println(driver.getTitle());
-		
-		
-		
+
 	}
-	
-	
-	
+
 	@AfterTest
-	public void teardown()
-	{
-		if(driver!=null)
-		driver.close();
+	public void teardown() {
+		if (driver != null)
+			driver.close();
 	}
-	
-	
+
 	@DataProvider(name = "DP1")
-    public Object[][] createData() {
+	public Object[][] createData() {
 		final String dir = System.getProperty("user.dir");
-        System.out.println("current dir = " + dir);
-		return Wrapper.getTableArray("C:\\Users\\sahur\\lunaworkspace\\selenium"+"/src/test/resources/testdata/LINKS.xls", "Pages", "Pages");
-		
-//        Object[][] retObjArr={{"001","Jack","London"},
-//                            {"002","John","New York"},
-//                            {"003","Mary","Miami"},
-//{"004","George","california"}};
-//        return(retObjArr);
-    }
-	
-	
+		System.out.println("current dir = " + dir);
+		return Wrapper.getTableArray(
+				"C:\\Users\\sahur\\lunaworkspace\\selenium"
+						+ "/src/test/resources/testdata/LINKS.xls", "Pages",
+				"Pages");
+
+	}
+
 }
